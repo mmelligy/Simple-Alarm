8ef38881e78db8aa082abb6376d092bfc8af76a1
mmelligy <mahmoud-elmelligy@elarabygroup.com git config --global user.email mahmoud-elmelligy@elarabygroup.com>
2019-12-03 17:53:24 +0200

    Initial Commit

---
+++ Simple Alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Alarm
+//
+//  Created by Mahmoud El-Melligy on 12/3/19.
+//  Copyright © 2019 Mahmoud El-Melligy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
